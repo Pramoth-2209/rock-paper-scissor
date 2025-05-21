da749e0b5ab87b8818ad4352a0fab85b5b056d15
Pramoth-2209 <pramothk577@gmail.com>
2025-05-21 10:26:29 +0530

    drop down add

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ let b=0;
 
 function play(){
     let bot=document.getElementById('bot');
-    let bval=Math.floor(Math.random()*3);  
-    let uval=Number(document.getElementById("userinp").value); 
+    let lists=['rock','paper','scicssor']
+    let bval=Math.floor(Math.random()*lists.length);  
+    let uval=document.getElementById("userinp").value; 
     let output=document.getElementById("output");
     let point=document.getElementById("point");
     let point2=document.getElementById("point2");
     let output2=document.getElementById("output2")
-    bot.innerText=bval;
-    console.log(bval);
+    bot.innerText=lists[bval];
+    console.log(lists[bval]);
     console.log(uval)
-    if(bval === uval){
+    if(lists[bval] === uval){
         output.innerText="draw";
     }
     else if(
-        (uval===0 && bval===2) || (uval==1 && bval==0) || (uval===2 && bval===1)
+        (uval==="rock" && lists[bval]==="scicssor") || (uval==="paper" && lists[bval]==="rock") || (uval==="scicssor" && lists[bval]==="paper")
     ){
         output.innerText="you won";
         
@@ -25,6 +26,7 @@ function play(){
         if(a>=5)
         {
             output2.innerText="you won the match";
+            point.innerText="0";
         }
 
     }
@@ -34,12 +36,14 @@ function play(){
         point2.innerText=b;
         if(b>=5){
             output2.innerText="opponent won the match";
+            point2.innerText="0";
         }
 
     }
-    if(uval==" "){
-        alert("please enter value")
+    if(uval=="select"){
+        alert("please select value like 'rock','paper','scicssor' ")
     }
+
     
 }
 
